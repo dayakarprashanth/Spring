9f27ff030482a0603981a6952d146fb3f1724efe
dayakarr <dayakarr@D157-44524.sce.uhcl.edu>
2020-03-27 22:51:51 -0500

    One to One Bi-Directional

---
+++ Hibernate/hb-02-one-to-one-bi/src/com/example/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,54 @@
+package com.example.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.example.hibernate.demo.entity.Instructor;
+import com.example.hibernate.demo.entity.InstructorDetail;
+import com.example.hibernate.demo.entity.Student;
+
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		//create Session factory
+		SessionFactory factory = new Configuration()
+								.configure()
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+
+		//create session
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+						
+			//start transaction
+			session.beginTransaction();
+			
+			int theId=2;
+			
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			session.delete(tempInstructorDetail);
+						
+						
+			// commit the transaction
+			session.getTransaction().commit();
+	}
+		catch (Exception exc) {
+			
+			exc.printStackTrace();
+			
+		}
+		finally {
+			
+			session.close();
+			factory.close();
+		}
+	}
+}
---
+++ Hibernate/hb-02-one-to-one-bi/src/com/example/hibernate/demo/DeleteInstructorDetailNotInstructorDemo.java
@@ -0,0 +1,58 @@
+package com.example.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.example.hibernate.demo.entity.Instructor;
+import com.example.hibernate.demo.entity.InstructorDetail;
+import com.example.hibernate.demo.entity.Student;
+
+
+public class DeleteInstructorDetailNotInstructorDemo {
+
+	public static void main(String[] args) {
+		
+		//create Session factory
+		SessionFactory factory = new Configuration()
+								.configure()
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+
+		//create session
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+						
+			//start transaction
+			session.beginTransaction();
+			
+			int theId=3;
+			
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			//break the Association
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			
+			
+			session.delete(tempInstructorDetail);
+						
+						
+			// commit the transaction
+			session.getTransaction().commit();
+	}
+		catch (Exception exc) {
+			
+			exc.printStackTrace();
+			
+		}
+		finally {
+			
+			session.close();
+			factory.close();
+		}
+	}
+}
